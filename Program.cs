20c4f2945cb958e223e5dd876f286f06a509c262
EkalVidhyardhi <76987900+EkalVidhyardhi@users.noreply.github.com>
2021-01-05 01:05:02 -0500

    Initial commint for HelloWorld

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
